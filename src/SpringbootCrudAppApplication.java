c1f42081ab2647924febec7be8bb5298bff2a1cf
BilalAM <bilal.asif.97@gmail.com>
2018-01-27 16:59:30 +0500

    change

--- src/SpringbootCrudAppApplication.java
+++ src/SpringbootCrudAppApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootCrudAppApplication {
 
-	
-	//main point
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootCrudAppApplication.class, args);
 	}
