b5b77201640068f5377793cff3101662bc64bce8
BilalAM <bilal.asif.97@gmail.com>
2018-01-29 14:16:54 +0500

    added new item logic

--- src/main/java/spring/controllers/ItemController.java
+++ src/main/java/spring/controllers/ItemController.java
@@ -9,21 +9,22 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import spring.models.Item;
 import spring.services.ItemService;
 
-@RestController
+@Controller
 public class ItemController {
 
 	@Autowired
 	private ItemService itemService;
 
-	@RequestMapping("/items")
-	public List<Item> getAllItems(Model model) {
+	@RequestMapping(value = "/items", method = RequestMethod.GET)
+	public String products(Model model) {
 		model.addAttribute("items", itemService.getAllItems());
-		return itemService.getAllItems();
+		return "products";
 	}
 
 	@RequestMapping("/item/{id}")
@@ -31,9 +32,19 @@ public class ItemController {
 		return itemService.getItem(id);
 	}
 
-	@RequestMapping(method = RequestMethod.POST, value = "/items")
-	public void addItem(@RequestBody Item item) {
+	@RequestMapping(method = RequestMethod.GET, value = "/create")
+	public String create() {
+		return "newproduct";
+	}
+
+	@RequestMapping(method = RequestMethod.POST, value = "/new")
+	public String addItem(@RequestParam String name , @RequestParam int quantity , @RequestParam double price) {
+		Item item = new Item();
+		item.setName(name);
+		item.setPrice(price);
+		item.setQuantity(quantity);
 		itemService.addItem(item);
+		return "redirect:/items";
 	}
 
 	@RequestMapping(method = RequestMethod.PUT, value = "/item/{id}")
