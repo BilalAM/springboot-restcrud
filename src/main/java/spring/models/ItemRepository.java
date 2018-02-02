41519be19d58bbf971fa1d0f6f0a9f4248b96289
BilalAM <bilal.asif.97@gmail.com>
2018-02-02 22:09:04 +0500

    changed to JpaRepository<> for better configuration with JPA

--- src/main/java/spring/models/ItemRepository.java
+++ src/main/java/spring/models/ItemRepository.java
@@ -1,7 +1,8 @@
 package spring.models;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ItemRepository extends CrudRepository<Item, Integer> {
-
+public interface ItemRepository extends JpaRepository<Item, Integer> {
+			
 }
