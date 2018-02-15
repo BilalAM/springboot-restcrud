8074a7c0a969210d2e26e9a96338cfb66e4ec8cc
BilalAM <bilal.asif.97@gmail.com>
2018-02-16 00:01:18 +0500

    inherited command line runner to test reviews and item services

--- src/main/java/spring/SpringbootCrudAppApplication.java
+++ src/main/java/spring/SpringbootCrudAppApplication.java
@@ -1,14 +1,36 @@
 package spring;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringbootCrudAppApplication {
+import spring.models.Item;
+import spring.models.Review;
+import spring.services.ItemService;
+import spring.services.ReviewService;
 
-	
-	//main point
+@SpringBootApplication
+public class SpringbootCrudAppApplication implements CommandLineRunner {
+
+	@Autowired
+	private ItemService itemService;
+
+	@Autowired
+	private ReviewService reviewService;
+
+	// main point
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootCrudAppApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		/*
+		 * Item item = itemService.getItem(2); for(Review r : item.getReviews()) {
+		 * System.out.println(r.getReviewText()); }
+		 */
+
 	}
 }
